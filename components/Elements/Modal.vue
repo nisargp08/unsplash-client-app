dc02ff6b13b8a10fc8dce9cfe7165f89ea4c7a73
Nisarg Patel <nisargp42@gmail.com>
2021-04-18 23:51:32 -0400

    Modal :: Componentized

---
+++ components/Elements/Modal.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="modal-container relative">
+    <!-- Modal window -->
+    <div
+      class="modal-window flex flex-col items-center justify-center fixed inset-0"
+    >
+      <!-- Modal backdrop -->
+      <div
+        class="modal-backdrop absolute inset-0 bg-gray-900 bg-opacity-50"
+        role="button"
+        @click="closeModal"
+      ></div>
+      <!-- Modal content -->
+      <div class="relative max-w-screen-sm w-full">
+        <div class="relative bg-white rounded-xl shadow-xl py-6 px-8 mx-4">
+          <slot></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // Close modal on escape key
+    window.addEventListener('keyup', (e) => {
+      if (e.code === 'Escape') {
+        this.closeModal()
+      }
+    })
+  },
+  methods: {
+    closeModal() {
+      this.$emit('closeModal')
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+.modal-window {
+  z-index: var(--zindex-modal);
+}
+</style>
--- components/Forms/AddPhoto.vue
+++ components/Forms/AddPhoto.vue
@@ -1,92 +1,73 @@
 <template>
-  <div class="modal-container relative">
-    <!-- Modal window -->
-    <div
-      class="modal-window flex flex-col items-center justify-center fixed inset-0"
-    >
-      <!-- Modal backdrop -->
-      <div
-        class="modal-backdrop absolute inset-0 bg-gray-900 bg-opacity-50"
-        role="button"
-        @click="closeModal"
-      ></div>
-      <!-- Modal content -->
-      <div class="relative max-w-screen-sm w-full">
-        <div class="relative bg-white rounded-xl shadow-xl py-6 px-8 mx-4">
-          <!-- Default state -->
-          <div class="form-wrapper">
-            <!-- Form Heading -->
-            <div class="flex items-center justify-between">
-              <h3 class="text-2xl font-medium">Add a new photo</h3>
-              <button class="text-gray-500" @click="closeModal">
-                <icons-cancel :height="5" :width="5"></icons-cancel>
-              </button>
-            </div>
-            <!-- Form Fields-->
-            <div class="mt-6 space-y-5">
-              <div class="space-y-2">
-                <label class="block w-full font-medium text-sm" for="photoLabel"
-                  >Label</label
-                >
-                <input
-                  id="photoLabel"
-                  v-model="inputFile.label"
-                  class="block w-full text-sm border border-gray-300 rounded-lg py-2 px-4 shadow-sm transition focus:outline-none focus:border-gray-500"
-                  type="text"
-                  name="photoLabel"
-                  placeholder="Photo Label"
-                  required
-                />
-              </div>
-              <div class="space-y-2">
-                <label class="block w-full font-medium text-sm" for="photoUrl"
-                  >Photo URL</label
-                >
-                <input
-                  id="photoUrl"
-                  v-model="inputFile.url"
-                  class="block w-full text-sm border border-gray-300 rounded-lg py-2 px-4 shadow-sm transition focus:outline-none focus:border-gray-500"
-                  type="text"
-                  name="photoUrl"
-                  placeholder="https://www.example.com"
-                  required
-                />
-              </div>
-            </div>
-            <!-- Error panel -->
-            <div class="mt-4 text-sm text-red-500 font-medium">
-              <ul>
-                <li v-for="(error, index) in errMessages" :key="index">
-                  {{ error }}
-                </li>
-              </ul>
-            </div>
-            <!-- Form Submission -->
-            <div class="flex items-center space-x-2 justify-end">
-              <elements-button class="text-sm" @buttonClick="closeModal"
-                >Cancel</elements-button
-              >
-              <elements-button
-                type="submit"
-                class="text-sm"
-                color="green"
-                :disabled="disableCheck"
-                @buttonClick="uploadPhoto"
-                >Submit</elements-button
-              >
-            </div>
-          </div>
-          <!-- Loading state -->
-          <template v-if="apiIsLoading">
-            <icons-loading-spinner
-              :height="8"
-              :width="8"
-            ></icons-loading-spinner>
-          </template>
+  <elements-modal @closeModal="closeModal">
+    <!-- Default state -->
+    <div class="form-wrapper">
+      <!-- Form Heading -->
+      <div class="flex items-center justify-between">
+        <h3 class="text-2xl font-medium">Add a new photo</h3>
+        <button class="text-gray-500" @click="closeModal">
+          <icons-cancel :height="5" :width="5"></icons-cancel>
+        </button>
+      </div>
+      <!-- Form Fields-->
+      <div class="mt-6 space-y-5">
+        <div class="space-y-2">
+          <label class="block w-full font-medium text-sm" for="photoLabel"
+            >Label</label
+          >
+          <input
+            id="photoLabel"
+            v-model="inputFile.label"
+            class="block w-full text-sm border border-gray-300 rounded-lg py-2 px-4 shadow-sm transition focus:outline-none focus:border-gray-500"
+            type="text"
+            name="photoLabel"
+            placeholder="Photo Label"
+            required
+          />
+        </div>
+        <div class="space-y-2">
+          <label class="block w-full font-medium text-sm" for="photoUrl"
+            >Photo URL</label
+          >
+          <input
+            id="photoUrl"
+            v-model="inputFile.url"
+            class="block w-full text-sm border border-gray-300 rounded-lg py-2 px-4 shadow-sm transition focus:outline-none focus:border-gray-500"
+            type="text"
+            name="photoUrl"
+            placeholder="https://www.example.com"
+            required
+          />
         </div>
       </div>
+      <!-- Error panel -->
+      <div class="mt-4 text-sm text-red-500 font-medium">
+        <ul>
+          <li v-for="(error, index) in errMessages" :key="index">
+            {{ error }}
+          </li>
+        </ul>
+      </div>
+      <!-- Form Submission -->
+      <div class="flex items-center space-x-2 justify-end">
+        <elements-button class="text-sm" @buttonClick="closeModal"
+          >Cancel</elements-button
+        >
+        <elements-button
+          type="submit"
+          class="text-sm"
+          color="green"
+          :disabled="disableCheck"
+          @buttonClick="uploadPhoto"
+          >Submit</elements-button
+        >
+      </div>
     </div>
-  </div>
+    <!-- Loading state -->
+    <template v-if="apiIsLoading">
+      <icons-loading-spinner :height="8" :width="8"></icons-loading-spinner>
+    </template>
+  </elements-modal>
 </template>
 
 <script>
@@ -115,14 +96,6 @@ export default {
         return false
       }
     },
-  },
-  mounted() {
-    // Close modal on escape key
-    window.addEventListener('keyup', (e) => {
-      if (e.code === 'Escape') {
-        this.closeModal()
-      }
-    })
   },
   methods: {
     closeModal() {
@@ -159,9 +132,3 @@ export default {
   },
 }
 </script>
-
-<style lang="css" scoped>
-.modal-window {
-  z-index: var(--zindex-modal);
-}
-</style>
